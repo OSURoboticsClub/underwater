59cae6ffaab1d9adb70cd8cc307a70be375a65a9
drkitty <bumblingkitty@gmail.com>
2014-12-17 10:33:29 -0800

    Add (short) function documentation

--- manager/worker.h
+++ manager/worker.h
@@ -9,6 +9,11 @@ struct robot {
 };
 
 
+// Connect to the manager process.
 struct robot init();
+
+// Wait for manager to notify us of fresh sensor data; return it.
 struct sensor_data wait_for_sensor_data(struct robot* robot);
+
+// Put thruster data somewhere the manager will see it the next time it looks.
 void set_thruster_data(struct robot* robot, struct thruster_data* td);
