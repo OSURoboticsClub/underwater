64f5f72319a6d261bdd502c756b4a46d0c1a5403
Ryan Domres <ryan.domres@gmail.com>
2015-04-21 16:31:38 -0700

    Main Loop Functionality Implemented
    Main loop functions such as UDP commnication, state machine, and motor
    command implemented.

--- electrical/firmware/main.ino/main.h
+++ electrical/firmware/main.ino/main.h
@@ -6,3 +6,16 @@
 #include "serialUnderwaterROV.h"
 
 int watchdogCounter;
+
+void EStop() {
+	ST1.motor(1, 0);
+	ST1.motor(2, 0);
+
+	ST1.motor(1, 0);
+	ST1.motor(2, 0);
+
+	ST1.motor(1, 0);
+	ST1.motor(2, 0);
+
+	while (1);
+}
--- electrical/firmware/main.ino/main.ino.ino
+++ electrical/firmware/main.ino/main.ino.ino
@@ -7,56 +7,61 @@
 #include <Wire.h>
 
 void setup() {
-  // Setup UDP Communication to control computer
-  // Setup communication with Saberteeth
-  Serial1.begin(9600);
-  Serial2.begin(9600);
-  Serial3.begin(9600);
-  
-  // Initialize IMU
-  accelBegin();
-  magnoBegin();
-  gyroBegin();
-  
-  current_state = idle;
-  watchdogCounter = 0;
+	// Setup UDP Communication to control computer
+	ethernetSetup();
+	// Setup communication with Saberteeth
+	Serial1.begin(9600);
+	Serial2.begin(9600);
+	Serial3.begin(9600);
+	
+	// Initialize IMU
+	IMUSetup();
+	
+	current_state = idle;
+	watchdogCounter = 0;
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    // We have communication with the master
-    watchdogCounter = 0;
-    // TODO: Authentication packet
-    // TODO: Parse incoming packet
-    current_state = commanded_state;
-    if (commanded_state == talk or commanded_state == run) {
-      // Reads sensors
-      accelRead();
-      magnoRead();
-      gyroRead();
-      // TODO: Populate return packet
-    }
-    if (commanded_state == run) {
-      /* TODO: Check error
-      if (check_error()) {
-        current_state = error;
-      }
-      */
-      if (current_state == run) {
-        // We're still in run so motors are safe
-        // TODO: Command Saberteeth
-      }
-    }
-    //TODO: Return packet to master
-  }
-  else {
-    // No packet was recieved, increment counter and wait
-    watchdogCounter++;
-    if (watchdogCounter > 1000) {
-      // We've lost communication
-      current_state = error;
-      // TODO: Return error packet
-    }
-    delay(10);
-  }
+	if (ethernetRead()) {
+		// We have communication with the master
+		watchdogCounter = 0;
+		// TODO: Authentication packet
+		// TODO: Parse incoming packet
+		current_state = commanded_state;
+		if (commanded_state == talk or commanded_state == run) {
+			// Reads sensors
+			accelRead();
+			magnoRead();
+			gyroRead();
+			IMUPacketize();
+		}
+		if (commanded_state == run) {
+			/* TODO: Check error
+			if (check_error()) {
+				current_state = error;
+			}
+			*/
+			if (current_state == run) {
+				// We're still in run so motors are safe
+				ST1.motor(1, motorCommand.frontLeft);
+				ST1.motor(2, motorCommand.frontRight);
+
+				ST2.motor(1, motorCommand.backLeft);
+				ST2.motor(2, motorCommand.backRight);
+
+				ST3.motor(1, motorCommand.vertLeft);
+				ST3.motor(2, motorCommand.vertRight);
+			}
+		}
+		ethernetWrite();
+	}
+	else {
+		// No packet was recieved, increment counter and wait
+		watchdogCounter++;
+		if (watchdogCounter > 1000) {
+			// We've lost communication
+			current_state = error;
+			// TODO: Return error packet
+		}
+	}
 }
